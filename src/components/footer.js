dcdd099eb8c2b53d6f5985e37115467de9ab2f0e
Carlos Moreno <carlos_moreno_mc@hotmail.com>
2020-04-14 15:47:36 +0200

    footer hooks

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer hero is-medium is-primary is-bold">
-                <div className="content has-text-centered">
-                    <p>
-                        Movie Finder App by <a href="/">Carlos Moreno</a>. API provided by
-                        <a href="http://www.omdbapi.com/" target="_blank" rel="noopener noreferrer"> OMBb</a>
-                    </p>
-                </div>
-            </footer>
-        )
-    }
+export const Footer = () => {
+    return (
+        <footer className="footer hero is-medium is-primary is-bold">
+            <div className="content has-text-centered">
+                <p>
+                    Movie Finder App by <a href="/">Carlos Moreno</a>. API provided by
+                    <a href="http://www.omdbapi.com/" target="_blank" rel="noopener noreferrer"> OMBb</a>
+                </p>
+            </div>
+        </footer>
+    )
 }
